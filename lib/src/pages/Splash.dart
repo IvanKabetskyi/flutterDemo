a14d7bbbe910dbb070d8135d70ba7f6bebc5cc73
Ivan <ivan@Admins-Mac-mini.local>
2020-01-16 13:13:56 +0200

    CREATE: splash page

--- lib/src/pages/Splash.dart
+++ lib/src/pages/Splash.dart
@@ -1,9 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  SplashState createState() => SplashState();
+}
+
+class SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 300), () {
+      Navigator.of(context).pushNamed('/login');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Navigator.pushNamed(context, '/login');
     return Scaffold();
   }
 }
