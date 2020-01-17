de176897dc4c8a852c3ec8f809d50624b48b8c6b
Ivan <ivan@Admins-Mac-mini.local>
2020-01-17 18:02:54 +0200

    CREATE: model for me request

--- lib/src/models/app_state.dart
+++ lib/src/models/app_state.dart
@@ -3,25 +3,66 @@ class AppState {
   final String refreshToken;
   final Map<String, dynamic> carrier;
 
-  AppState({this.accessToken, this.refreshToken, this.carrier});
+  final int driverId;
+  final String driverNote;
+  final String firstName;
+  final String lastName;
+  final bool isOwner;
+  final Map<String, dynamic> truck;
+
+  AppState(
+      {this.accessToken,
+      this.refreshToken,
+      this.carrier,
+      this.driverId,
+      this.driverNote,
+      this.firstName,
+      this.lastName,
+      this.isOwner,
+      this.truck});
 
   AppState copyWith(
           {String accessToken,
           String refreshToken,
-          Map<String, dynamic> carrier}) =>
+          Map<String, dynamic> carrier,
+          int driverId,
+          String driverNote,
+          String firstName,
+          String lastName,
+          bool isOwner,
+          Map<String, dynamic> truck}) =>
       AppState(
           accessToken: accessToken ?? this.accessToken,
           refreshToken: refreshToken ?? this.refreshToken,
-          carrier: carrier ?? this.carrier);
+          carrier: carrier ?? this.carrier,
+          driverId: driverId ?? this.driverId,
+          driverNote: driverNote ?? this.driverNote,
+          firstName: firstName ?? this.firstName,
+          lastName: lastName ?? this.lastName,
+          isOwner: isOwner ?? this.isOwner,
+          truck: truck ?? this.truck);
 
   static AppState fromJson(dynamic json) => AppState(
-      accessToken: json['accessToken'] as String,
-      refreshToken: json['refreshToken'] as String,
-      carrier: json['carrier'] as Map<String, dynamic>);
+        accessToken: json['accessToken'] as String,
+        refreshToken: json['refreshToken'] as String,
+        carrier: json['carrier'] as Map<String, dynamic>,
+        driverId: json['driverId'] as int,
+        driverNote: json['driverNote'] as String,
+        firstName: json['firstName'] as String,
+        lastName: json['lastName'] as String,
+        isOwner: json['isOwner'] as bool,
+        truck: json['truck'] as Map<String, dynamic>,
+      );
 
   dynamic toJson() => {
         'accessToken': accessToken,
         'refreshToken': refreshToken,
-        'carrier': carrier
+        'carrier': carrier,
+        'driverId': driverId,
+        'driverNote': driverNote,
+        'firstName': firstName,
+        'lastName': lastName,
+        'isOwner': isOwner,
+        'truck': truck
       };
 }
