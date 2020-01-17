3cef40f2f6caf101bb916129d3e17eb2f3599b64
Ivan <ivan@Admins-Mac-mini.local>
2020-01-17 17:44:16 +0200

    UPDATE: login and home back button

--- lib/src/pages/Home.dart
+++ lib/src/pages/Home.dart
@@ -10,26 +10,31 @@ class HomePage extends StatelessWidget {
     GlobalKey<FormState> _homeKey =
         GlobalKey<FormState>(debugLabel: '_homeScreenkey');
     // Navigator.pushNamed(context, '/login');
-    return Scaffold(
-      key: _homeKey,
-      body: Center(
-          child: RaisedButton(
-        child: StoreConnector<AppState, dynamic>(
-            converter: (store) => {
-                  'accessToken': store.state.accessToken.toString(),
-                  'refreshToken': store.state.accessToken.toString()
-                },
-            builder: (context, tokens) => Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('accessToken: ${tokens["accessToken"]}'),
-                      Text('refreshToken: ${tokens["refreshToken"]}')
-                    ])),
-        onPressed: () {
-          Navigator.pushNamed(context, '/chat');
-        },
-      )),
-      drawer: DrawerComponent(),
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        key: _homeKey,
+        body: Center(
+            child: RaisedButton(
+          child: StoreConnector<AppState, dynamic>(
+              converter: (store) => {
+                    'accessToken': store.state.accessToken.toString(),
+                    'refreshToken': store.state.accessToken.toString()
+                  },
+              builder: (context, tokens) => Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('accessToken: ${tokens["accessToken"]}'),
+                        Text('refreshToken: ${tokens["refreshToken"]}')
+                      ])),
+          onPressed: () {
+            Navigator.pushNamed(context, '/chat');
+          },
+        )),
+        drawer: DrawerComponent(),
+      ),
     );
   }
 }
--- lib/src/pages/Login.dart
+++ lib/src/pages/Login.dart
@@ -26,146 +26,151 @@ class LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onTap: clickOutsideTextFeild,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/bg_pic_login.png'),
-                fit: BoxFit.cover),
-          ),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Container(),
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.85,
-                  child: Container(
-                    height: 70,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('images/logo.png'),
-                            fit: BoxFit.contain)),
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: GestureDetector(
+          onTap: clickOutsideTextFeild,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/bg_pic_login.png'),
+                  fit: BoxFit.cover),
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(),
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                FractionallySizedBox(
-                  widthFactor: .8,
-                  child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        InputContainer(
-                            key: Key('Company'),
-                            myController: company,
-                            title: 'Company',
-                            icon: Icons.contact_mail,
-                            errMessage: companyError,
-                            validation: companyValidation,
-                            maxlength: 3),
-                        InputContainer(
-                            key: Key('Email'),
-                            myController: email,
-                            title: 'Email',
-                            icon: Icons.email,
-                            errMessage: emailError,
-                            validation: emailValidation,
-                            keyboardType: TextInputType.emailAddress),
-                        InputContainer(
-                            key: Key('Password'),
-                            myController: password,
-                            title: 'Password',
-                            icon: Icons.lock,
-                            obscureText: true,
-                            errMessage: pwdError,
-                            validation: pwdValidation),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Wrap(
-                            runSpacing: 4.0,
-                            children: <Widget>[
-                              Text(
-                                'By signing in, you agree to the ',
-                                style: TextStyle(
-                                    color: Color(0xFFB0BEC5), fontSize: 14),
-                              ),
-                              InkWell(
-                                child: Text(
-                                  'private police.',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      decoration: TextDecoration.underline,
-                                      fontSize: 14),
-                                ),
-                                onTap: _launchPrivatePolice,
-                              )
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        StoreConnector<AppState, Function>(
-                          converter: (store) {
-                            return (company, email, password, context) =>
-                                store.dispatch(setAccessToken(
-                                    company, email, password, context));
-                          },
-                          builder: (_, login) => RawMaterialButton(
-                            fillColor: Color(0xFF28bbff),
-                            splashColor: Color(0xFF0386d2),
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(8.0))),
-                            child: Container(
-                              height: 60,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(6),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'Log In',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
-                                ),
-                              ),
-                            ),
-                            onPressed: () async {
-                              login(company.text, email.text, password.text,
-                                  context);
-                            },
-                          ),
-                        ),
-                      ]),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Column(children: <Widget>[
-                  Text(
-                    'Powered by Rhino Technologies',
-                    style: TextStyle(color: Color(0xFFB0BEC5)),
-                  ),
-                  Text(
-                    'version 1.20.8-testing',
-                    style: TextStyle(color: Color(0xFFB0BEC5), fontSize: 11),
+                  FractionallySizedBox(
+                    widthFactor: 0.85,
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('images/logo.png'),
+                              fit: BoxFit.contain)),
+                    ),
                   ),
                   SizedBox(
-                    height: 20,
-                  )
+                    height: 30,
+                  ),
+                  FractionallySizedBox(
+                    widthFactor: .8,
+                    child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InputContainer(
+                              key: Key('Company'),
+                              myController: company,
+                              title: 'Company',
+                              icon: Icons.contact_mail,
+                              errMessage: companyError,
+                              validation: companyValidation,
+                              maxlength: 3),
+                          InputContainer(
+                              key: Key('Email'),
+                              myController: email,
+                              title: 'Email',
+                              icon: Icons.email,
+                              errMessage: emailError,
+                              validation: emailValidation,
+                              keyboardType: TextInputType.emailAddress),
+                          InputContainer(
+                              key: Key('Password'),
+                              myController: password,
+                              title: 'Password',
+                              icon: Icons.lock,
+                              obscureText: true,
+                              errMessage: pwdError,
+                              validation: pwdValidation),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Wrap(
+                              runSpacing: 4.0,
+                              children: <Widget>[
+                                Text(
+                                  'By signing in, you agree to the ',
+                                  style: TextStyle(
+                                      color: Color(0xFFB0BEC5), fontSize: 14),
+                                ),
+                                InkWell(
+                                  child: Text(
+                                    'private police.',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        decoration: TextDecoration.underline,
+                                        fontSize: 14),
+                                  ),
+                                  onTap: _launchPrivatePolice,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          StoreConnector<AppState, Function>(
+                            converter: (store) {
+                              return (company, email, password, context) =>
+                                  store.dispatch(setAccessToken(
+                                      company, email, password, context));
+                            },
+                            builder: (_, login) => RawMaterialButton(
+                              fillColor: Color(0xFF28bbff),
+                              splashColor: Color(0xFF0386d2),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(8.0))),
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(6),
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    'Log In',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                              onPressed: () async {
+                                login(company.text, email.text, password.text,
+                                    context);
+                              },
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Column(children: <Widget>[
+                    Text(
+                      'Powered by Rhino Technologies',
+                      style: TextStyle(color: Color(0xFFB0BEC5)),
+                    ),
+                    Text(
+                      'version 1.20.8-testing',
+                      style: TextStyle(color: Color(0xFFB0BEC5), fontSize: 11),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    )
+                  ]),
                 ]),
-              ]),
+          ),
         ),
       ),
     );
